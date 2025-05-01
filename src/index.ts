c9ff72ad42783fbf915ebe42062a4e3a9fc01dac
rammfall <15bogdan17@gmail.com>
2025-05-01 14:30:32 +0300

    Update template

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import fastify from 'fastify';
+import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
+import fastifyCors from '@fastify/cors';
+import fastifyCookie from '@fastify/cookie';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
+import fastifyMultipart from '@fastify/multipart';
+import fastifyStatic from '@fastify/static';
 
 const server = fastify({
   logger: {
@@ -10,6 +17,17 @@ const server = fastify({
       },
     },
   },
+}).withTypeProvider<TypeBoxTypeProvider>();
+
+await server.register(fastifyCors);
+await server.register(fastifyCookie);
+await server.register(fastifySwagger);
+await server.register(fastifySwaggerUi);
+await server.register(fastifyStatic, {
+  root: `${process.cwd()}/public`,
+});
+await server.register(fastifyMultipart, {
+  attachFieldsToBody: 'keyValues',
 });
 
 server
